8ba865877e19061f56d59a9e8ab02954302a23c7
tlanders <tlanders@landersconsulting.biz>
2018-08-01 10:27:32 -0500

    added new reactive web project

---
+++ lsb-web/src/main/java/biz/lci/learningspringboot/lsbweb/LsbWebApplication.java
@@ -0,0 +1,12 @@
+package biz.lci.learningspringboot.lsbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsbWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsbWebApplication.class, args);
+	}
+}
