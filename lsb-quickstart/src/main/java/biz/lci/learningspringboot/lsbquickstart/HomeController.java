541e56ba3f7569d878e4d4f877e0f87624888cb3
tlanders <tlanders@landersconsulting.biz>
2018-08-05 15:14:05 -0500

    added actuator

--- lsb-quickstart/src/main/java/biz/lci/learningspringboot/lsbquickstart/HomeController.java
+++ lsb-quickstart/src/main/java/biz/lci/learningspringboot/lsbquickstart/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping
+    @GetMapping("/")
     public String home(@RequestParam(required = false, defaultValue = "") String name) {
         return "Hello " + name + "!";
     }
